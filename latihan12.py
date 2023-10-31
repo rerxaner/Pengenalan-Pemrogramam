a62cf175cdde42bc60b7edf4401d243a499beca5
rerxaner <erxanner@gmal.com>
2023-10-31 08:21:30 +0700

    perubahan dua

---
+++ latihan12.py
@@ -0,0 +1,3 @@
+nama  = "Rahman"
+
+print("Halo semua nama saya",nama,"Saya","suka","ngoding", sep="/")
---
+++ quiz1.py
@@ -0,0 +1,30 @@
+# NPM(dalam bentuk komentar)
+# (tambahan karakter new line)
+# No Struk: # 1
+# Nama Kasir:
+# Barang 1:
+# Harga Barang Rp:
+# Barang 2:
+# Harga Barang 2:
+# Barang 3:
+# Harga Barang 3:
+# --------------------
+# Total Bayar Rp 
+# (Tamabh karakter new line)
+# TERIMA KASIH SUDAH BERBELANJA DI TOKO KAMI
+
+# npm : 2355201085
+no_struk = int(input("No Struk : "))
+nama_kasir = input("Nama Kasir : ")
+barang1 = input("Barang 1 : ")
+harag_barang_1 = int(input("Harga Barang 1 Rp : "))
+barang2 = input("Barang 2 : ")
+harag_barang_2 = int(input("Harga Barang 2 Rp : "))
+barang3 = input("Barang 3 : ")
+harag_barang_3 = int(input("Harga Barang 3 Rp : "))
+
+print("----------------------------------------------")
+print("Total Bayar Rp :", harag_barang_1 + harag_barang_2 + harag_barang_3, "\n")
+print("TERIMA KASIH SUDAH BERBELANJA DI TOKO KAMI")
+
+
